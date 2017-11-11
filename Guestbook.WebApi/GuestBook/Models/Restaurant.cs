3a688d1a84b6131931ff1d672c74574a35919ea1
Roman <rash261292@gmail.com>
2017-11-11 22:21:55 +0600

    added classes model

---
+++ Guestbook.WebApi/GuestBook/Models/Restaurant.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GuestBook.Models
+{
+    public class Restaurant : IModel
+    {
+        [Required]
+        [StringLength(32, MinimumLength = 1)]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
+        public string Address { get; set; }
+
+        [Required]
+        public List<Review> Reviews { get; set; }
+
+        public Restaurant()
+        {
+            Id = IdentityGenerator.NewId();
+        }
+    }
+}
