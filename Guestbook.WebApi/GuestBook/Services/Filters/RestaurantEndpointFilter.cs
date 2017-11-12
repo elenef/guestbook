c8bff198c16ade5470298823b94d03aad5a6a9e2
Roman <rash261292@gmail.com>
2017-11-12 20:01:54 +0600

    added empty filters

---
+++ Guestbook.WebApi/GuestBook/Services/Filters/RestaurantEndpointFilter.cs
@@ -0,0 +1,17 @@
+﻿using GuestBook.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuestBook.Services.Filters
+{
+    public class RestaurantEndpointFilter : BaseEndpoinFilter<RestaurantFilterContract, Restaurant>
+    {
+        public override Task<IQueryable<Restaurant>> ApplyAsync(IQueryable<Restaurant> queryable,
+            RestaurantFilterContract filter)
+        {
+            var query = queryable;
+
+            return Task.FromResult(query);
+        }
+    }
+}
---
+++ Guestbook.WebApi/GuestBook/Services/Filters/ReviewEndpointFilter.cs
@@ -0,0 +1,17 @@
+﻿using GuestBook.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuestBook.Services.Filters
+{
+    public class ReviewEndpointFilter : BaseEndpoinFilter<ReviewFilterContract, Review>
+    {
+        public override Task<IQueryable<Review>> ApplyAsync(IQueryable<Review> queryable,
+            ReviewFilterContract filter)
+        {
+            var query = queryable;
+
+            return Task.FromResult(query);
+        }
+    }
+}
