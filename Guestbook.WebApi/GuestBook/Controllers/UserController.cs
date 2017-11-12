74bacf9b20f063b845559e4b1b0c63538ddd3bd0
Roman <rash261292@gmail.com>
2017-11-12 20:00:46 +0600

    added controllers

---
+++ Guestbook.WebApi/GuestBook/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using GuestBook.Models;
+using GuestBook.Models.Contracts;
+using GuestBook.Services;
+using GuestBook.Services.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuestBook.Controllers
+{
+    [Route("/users")]
+    public class UserController : ApiControllerBase<UserContract, EditUserContract, UserFilterContract, User>
+    {
+        public UserController(IEndpointService<UserContract, EditUserContract, UserFilterContract, User> endpointService) : base(endpointService)
+        {
+
+        }
+    }
+
+}
+
