19204d93b793f94faa7eac7fa7744d6362051128
elenef <elenaefremova9750@gmail.com>
2017-11-28 00:33:13 +0600

    ng serve fixed

--- Guestbook.Web/src/app/app-routing.module.ts
+++ Guestbook.Web/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard, AdminGuard } from './api/index';
+import { AuthGuard, AdminGuard } from './api/guards/index';
 import { PageDetailsComponent } from "./shared/components/page-details/page-details.component";
 // tslint:disable-next-line:max-line-length
 
 const appRoutes: Routes = [
 
 
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '/',  component: , },
   { path: '**', redirectTo: '' }
 
 ];
