a4afb6809abf906fd6f8ece0c54c670b62ce4d40
Roman <rash261292@gmail.com>
2017-12-11 00:10:58 +0600

    dev: changed MapperProfile

--- Guestbook.WebApi/GuestBook/Mapper/MapperProfile.cs
+++ Guestbook.WebApi/GuestBook/Mapper/MapperProfile.cs
@@ -14,12 +14,13 @@ namespace GuestBook.WebApi.Mapper
             CreateMap<Restaurant, RestaurantContract>()
                 .ForMember(d => d.ReviewIds, opt => opt.Ignore());
 
+
             CreateMap<EditReviewContract, Review>();
 
             CreateMap<Review, ReviewContract>()
                 .ForMember(d => d.Created, opt => opt.MapFrom(r => TimeConverter.ToUnixTime(r.Created)))
-                .ForMember(d => d.UserName, opt => opt.MapFrom(r => r.User.Name))
                 .ForMember(d => d.RestaurantName, opt => opt.MapFrom(r => r.Restaurant.Name));
+
 
             CreateMap<User, UserContract>();
 
